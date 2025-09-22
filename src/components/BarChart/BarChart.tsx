3c580ba790b06290315422580ad3fb079beeee5a
Stephan Kim <hhkimstar1226@gmail.com>
2025-09-22 17:48:16 +0900

    feat::BarChart 레이아웃 완성

--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -2,32 +2,37 @@ import { Theme } from '../../Theme/theme'
 import * as _ from './styled'
 import { Icon } from '../Icons/Icon'
 
+
+//props 뚫어야 함
 const BarChart = () => {
   return (
     <div>
-      <_.BarWrapper>
-        <_.BarContainer percent={58} pcs={3}>
-          <_.BarLabel>
-            <Icon size='XXS' color={Theme.Functional.Primary}>incomplete_circle</Icon>
-            <_.LabelText>Bitcoin (BTC)</_.LabelText>
-          </_.BarLabel>
-          <_.Bar bg={Theme.Functional.Primary} />
-        </_.BarContainer>
-        <_.BarContainer percent={25} pcs={3}>
-          <_.BarLabel>
-            <Icon size='XXS' color={Theme.Functional.Primary_2nd}>incomplete_circle</Icon>
-            <_.LabelText>Bitcoin (BTC)</_.LabelText>
-          </_.BarLabel>
-          <_.Bar bg={Theme.Functional.Primary_2nd} />
-        </_.BarContainer>
-        <_.BarContainer percent={17} pcs={3}>
-          <_.BarLabel>
-            <Icon size='XXS' color={Theme.Functional.Primary_3rd}>incomplete_circle</Icon>
-            <_.LabelText>Bitcoin (BTC)</_.LabelText>
-          </_.BarLabel>
-          <_.Bar bg={Theme.Functional.Primary_3rd} />
-        </_.BarContainer>
-      </_.BarWrapper>
+      <_.BarMainWrapper>
+        <_.BarDistribution>Distribution</_.BarDistribution>
+        <_.BarWrapper>
+          <_.BarContainer percent={58} pcs={3}>
+            <_.BarLabel>
+              <Icon size='XXS' color={Theme.Functional.Primary}>incomplete_circle</Icon>
+              <_.LabelText>58%</_.LabelText>
+            </_.BarLabel>
+            <_.Bar bg={Theme.Functional.Primary} />
+          </_.BarContainer>
+          <_.BarContainer percent={25} pcs={3}>
+            <_.BarLabel>
+              <Icon size='XXS' color={Theme.Functional.Primary_2nd}>incomplete_circle</Icon>
+              <_.LabelText>25%</_.LabelText>
+            </_.BarLabel>
+            <_.Bar bg={Theme.Functional.Primary_2nd} />
+          </_.BarContainer>
+          <_.BarContainer percent={17} pcs={3}>
+            <_.BarLabel>
+              <Icon size='XXS' color={Theme.Functional.Primary_3rd}>incomplete_circle</Icon>
+              <_.LabelText>17%</_.LabelText>
+            </_.BarLabel>
+            <_.Bar bg={Theme.Functional.Primary_3rd} />
+          </_.BarContainer>
+        </_.BarWrapper>
+      </_.BarMainWrapper>
     </div>
   )
 }
--- src/components/BarChart/styled.ts
+++ src/components/BarChart/styled.ts
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { Color, Gap, Radius, Text } from "../../Theme/theme";
+import { Color, Gap, Radius, Text, Theme } from "../../Theme/theme";
 
 type BarProps = {
   bg: string;
@@ -42,7 +42,19 @@ export const BarWrapper = styled.div`
   display: flex;
   align-items: flex-start;
   gap: ${Gap.Gap_4};
+  width: 100%;
+`
+
+export const BarMainWrapper = styled.div`
+  display: flex;
+  width: 35rem;
+  /* width 나중에 flex: {}로 수정 필요 */
+  flex-direction: column;
+  align-items: flex-start;
+  gap: ${Gap.Gap_8};
 `
 
 export const BarDistribution = styled.h1`
+  ${Text.Label.M};
+  color : ${Theme.Text.Text_10};
 `
