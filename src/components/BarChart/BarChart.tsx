08c72f8b05afa11e203dcf3dcb4bda37a950a6ab
Stephan Kim <hhkimstar1226@gmail.com>
2025-09-22 17:55:11 +0900

    feat::BarChart props 추가

--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -2,32 +2,38 @@ import { Theme } from '../../Theme/theme'
 import * as _ from './styled'
 import { Icon } from '../Icons/Icon'
 
+type BarChartProps = {
+  distribution: string;
+  percent1: number;
+  percent2: number;
+  percent3: number;
+  
+}
 
-//props 뚫어야 함
-const BarChart = () => {
+const BarChart = (props: BarChartProps) => {
   return (
     <div>
       <_.BarMainWrapper>
-        <_.BarDistribution>Distribution</_.BarDistribution>
+        <_.BarDistribution>{props.distribution}</_.BarDistribution>
         <_.BarWrapper>
-          <_.BarContainer percent={58} pcs={3}>
+          <_.BarContainer percent={props.percent1} pcs={3}>
             <_.BarLabel>
               <Icon size='XXS' color={Theme.Functional.Primary}>incomplete_circle</Icon>
-              <_.LabelText>58%</_.LabelText>
+              <_.LabelText>{props.percent1}%</_.LabelText>
             </_.BarLabel>
             <_.Bar bg={Theme.Functional.Primary} />
           </_.BarContainer>
-          <_.BarContainer percent={25} pcs={3}>
+          <_.BarContainer percent={props.percent2} pcs={3}>
             <_.BarLabel>
               <Icon size='XXS' color={Theme.Functional.Primary_2nd}>incomplete_circle</Icon>
-              <_.LabelText>25%</_.LabelText>
+              <_.LabelText>{props.percent2}%</_.LabelText>
             </_.BarLabel>
             <_.Bar bg={Theme.Functional.Primary_2nd} />
           </_.BarContainer>
-          <_.BarContainer percent={17} pcs={3}>
+          <_.BarContainer percent={props.percent3} pcs={3}>
             <_.BarLabel>
               <Icon size='XXS' color={Theme.Functional.Primary_3rd}>incomplete_circle</Icon>
-              <_.LabelText>17%</_.LabelText>
+              <_.LabelText>{props.percent3}%</_.LabelText>
             </_.BarLabel>
             <_.Bar bg={Theme.Functional.Primary_3rd} />
           </_.BarContainer>
