01b69ce197c633e66a4bd7f961bd78abe07a4a30
Stephan Kim <hhkimstar1226@gmail.com>
2025-09-22 17:32:44 +0900

    feat::BarChart 컴포넌트 구조 개선 및 스타일 수정

--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -1,9 +1,34 @@
 import { Theme } from '../../Theme/theme'
 import * as _ from './styled'
+import { Icon } from '../Icons/Icon'
 
 const BarChart = () => {
   return (
-      <_.Bar bg={Theme.Functional.Primary} percent={58} pcs={3} />
+    <div>
+      <_.BarWrapper>
+        <_.BarContainer percent={58} pcs={3}>
+          <_.BarLabel>
+            <Icon size='XXS' color={Theme.Functional.Primary}>incomplete_circle</Icon>
+            <_.LabelText>Bitcoin (BTC)</_.LabelText>
+          </_.BarLabel>
+          <_.Bar bg={Theme.Functional.Primary} />
+        </_.BarContainer>
+        <_.BarContainer percent={25} pcs={3}>
+          <_.BarLabel>
+            <Icon size='XXS' color={Theme.Functional.Primary_2nd}>incomplete_circle</Icon>
+            <_.LabelText>Bitcoin (BTC)</_.LabelText>
+          </_.BarLabel>
+          <_.Bar bg={Theme.Functional.Primary_2nd} />
+        </_.BarContainer>
+        <_.BarContainer percent={17} pcs={3}>
+          <_.BarLabel>
+            <Icon size='XXS' color={Theme.Functional.Primary_3rd}>incomplete_circle</Icon>
+            <_.LabelText>Bitcoin (BTC)</_.LabelText>
+          </_.BarLabel>
+          <_.Bar bg={Theme.Functional.Primary_3rd} />
+        </_.BarContainer>
+      </_.BarWrapper>
+    </div>
   )
 }
 
--- src/components/BarChart/styled.ts
+++ src/components/BarChart/styled.ts
@@ -1,34 +1,47 @@
 import styled from "@emotion/styled";
-import { Color, Gap, Radius } from "../../Theme/theme";
+import { Color, Gap, Radius, Text } from "../../Theme/theme";
 
 type BarProps = {
-    bg: string;
-    percent: number;
-    pcs: number;
+  bg: string;
+};
+
+type BarContainerProps = {
+  percent: number;
+  pcs: number;
 };
 
 export const Bar = styled.div<BarProps>`
-    border-radius: ${Radius.radius_6};
-    outline: 1px solid ${Color.GrayScale.Translucence.Translucence_10};
-    outline-offset: -1px;
-    height: 2rem;
-    background-color: ${(props) => props.bg};
-    width: calc((100% - ${Gap.Gap_4}*${(props) => props.pcs-1}) * 0.${(props) => props.percent});
+  border-radius: ${Radius.radius_6};
+  outline: 1px solid ${Color.GrayScale.Translucence.Translucence_10};
+  outline-offset: -1px;
+  height: 2rem;
+  background-color: ${(props) => props.bg};
+  width: 100%;
 `
 
 export const BarLabel = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${Gap.Gap_4};
 `
 
 export const LabelText = styled.span`
+  ${Text.Label.S};
+  color: ${Color.GrayScale.Translucence.Translucence_40};
 `
 
-export const LabelImg = styled.img`
-`
-
-export const BarContainer = styled.div`
+export const BarContainer = styled.div<BarContainerProps>`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: ${Gap.Gap_8};
+  width: calc((100% - ${Gap.Gap_4}*${(props) => props.pcs-1}) * 0.${(props) => props.percent});
 `
 
 export const BarWrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: ${Gap.Gap_4};
 `
 
 export const BarDistribution = styled.h1`
