ab2cf35d1e28cc31608930d6346f64347b40ab0f
wjdgur <i89155345@gmail.com>
2025-09-23 10:12:50 +0900

    feat::Layout SideBar 추가

--- src/Layout/Sidebar/Sidebar.tsx
+++ src/Layout/Sidebar/Sidebar.tsx
@@ -1,7 +1,39 @@
+import * as _ from "./styled";
+import SidebarLogo from "../../assets/LogoOnsidebar.svg";
+import { Icon } from "../../components/Icons/Icon";
+import { Theme } from "../../Theme/theme";
+import { NavMenu } from "../components/NavMenu/NavMenu";
+import github from "../../assets/github.svg";
+import { VIP } from "../components/VIP/VIP";
+
 export function Sidebar() {
   return (
-    <header>
-      <h1>Sidebar</h1>
-    </header>
+    <_.Container>
+      <_.Head>
+        <_.SidebarLogo src={SidebarLogo} />
+        <Icon size="M" color={Theme.Text.Text_20} fill={true}>
+          keyboard_double_arrow_left
+        </Icon>
+      </_.Head>
+      <_.BoxFill>
+        <NavMenu icon="dashboard">Dashboard</NavMenu>
+        <NavMenu icon="workspaces">Project</NavMenu>
+        <NavMenu icon="bar_chart">Analytics</NavMenu>
+        <NavMenu icon="person">User</NavMenu>
+      </_.BoxFill>
+      <_.Box>
+        <NavMenu icon="settings_b_roll">Settings</NavMenu>
+        <NavMenu icon="help">Help & Support</NavMenu>
+      </_.Box>
+      <_.BoxP>
+        <VIP />
+      </_.BoxP>
+      <_.Box>
+        <NavMenu>
+          <_.GitHub src={github} />
+          Help & Support
+        </NavMenu>
+      </_.Box>
+    </_.Container>
   );
 }
